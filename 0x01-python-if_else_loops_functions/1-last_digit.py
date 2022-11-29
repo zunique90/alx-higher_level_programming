6561497994ffe26f45be473eca6375b823a03b60
charles ezebuike <ezebuike.charles0@gmail.com>
2022-11-29 11:59:44 +0000

    tasks 0 & 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number >= 0:
+    num = number % 10
+else:
+    num = number % -10
+if num > 5:
+    print(f"Last digit of {number:d} is {num:d} and is greater than 5")
+elif num == 0:
+    print(f"Last digit of {number:d} is {num:d} and is 0")
+else:
+    print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks on Python "if, else, loops and functions"
