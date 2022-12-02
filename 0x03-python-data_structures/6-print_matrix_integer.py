5eb99c313f7ea96c8a45f39688cece233333eedc
charles ezebuike <ezebuike.charles0@gmail.com>
2022-12-02 16:05:40 +0000

    tasks 6,7,8 and 9

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for dup in matrix:
-        for i in dup:
-            print("{:d} ".format(i), end="")
+    for lists in matrix:
+        j = len(lists) - 1
+        for row in lists:
+            i = 0
+            if i == j:
+                print("{:d}".format(row))
+            else:
+                print("{:d}".format(row), end=" ")
+            i += 1
         print()
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-print_matrix_integer = __import__("6-print_matrix_integer").print_matrix_integer
+max_integer = __import__("9-max_integer").max_integer
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
