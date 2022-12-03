7bd4ef2b23e7cb5e8268528f7164edd98acff132
charles ezebuike <ezebuike.charles0@gmail.com>
2022-12-02 23:11:20 -0800

    correction for task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for lists in matrix:
-        j = len(lists) - 1
-        for row in lists:
-            i = 0
-            if i == j:
-                print("{:d}".format(row))
-            else:
-                print("{:d}".format(row), end=" ")
-            i += 1
-        print()
+    for i in matrix:
+        for j in range(0, len(i)):
+            print("{:d}".format(i[j]), end="")
+            if j != len(i) - 1:
+                print(" ", end="")
+        print("")
