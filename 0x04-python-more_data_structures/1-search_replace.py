7cde0778c61e167b12366903df2619abdeb6eaf2
charles ezebuike <ezebuike.charles0@gmail.com>
2022-12-07 18:55:52 +0000

    task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    newList = my_list.copy()
+
+    for i in range(len(newList)):
+        if newList[i] == search:
+            newList[i] = replace
+    return newList
