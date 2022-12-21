025a8d7dde61066e4a3c1f46cd2b180486542f28
charlesezebuike <ezebuike.charles0@gmail.com>
2022-12-21 01:14:28 +0000

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 3-square.py)"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    self.size = size
+
+    @property
+    def size(self):
+        """retrieves current size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size and handles errors"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
