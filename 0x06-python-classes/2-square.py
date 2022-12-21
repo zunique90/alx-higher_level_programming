157d7ee697307fee87c9da090f56e84e4a70ddda
charlesezebuike <ezebuike.charles0@gmail.com>
2022-12-21 00:34:34 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,6 +6,6 @@ def __init__(self, size=0):
     """Instantiation with optional size"""
     if type(size) != int:
         raise TypeError("size must be an integer")
-    if size < 0:
+    elif size < 0:
         raise ValueError("size must be >= 0")
     self.__size = size
