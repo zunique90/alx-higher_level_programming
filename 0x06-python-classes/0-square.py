41312d155b98eb6f8ef01cdd20dbd36c11717137
charlesezebuike <ezebuike.charles0@gmail.com>
2022-12-21 00:07:31 +0000

    task 0 & README

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""empty class that defines a square"""
+
+class Square:
+    """empty class"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is the directory for ALX project on Objects and Classes
