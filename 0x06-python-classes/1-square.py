b908404ecf4f63471c0a750d6b024eb2b239188f
charlesezebuike <ezebuike.charles0@gmail.com>
2022-12-21 00:20:25 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """Private instance attribute: size"""
+
+    def __init__(self, size):
+        self.__size = size
