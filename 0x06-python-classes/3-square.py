e4d9e51b8adb404470b25ae123770c27f208c3f4
charlesezebuike <ezebuike.charles0@gmail.com>
2022-12-21 00:56:51 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 2-square.py)"""
+
+
+class Square:
+    """Instantiation with optional size"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ returns the current square area"""
+        return self.__size ** 2
