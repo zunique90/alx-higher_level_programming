e7cf78b41eaa75fb9c2c1a021666682d7d7215ef
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-02 06:28:09 -0800

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,10 +5,13 @@
 class Rectangle:
     """class Rectangle"""
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Instantiation with optional width and height"""
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
