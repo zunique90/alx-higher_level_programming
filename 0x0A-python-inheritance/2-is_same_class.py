3e4ce6cf30beaa776e312870c7706549aae3c035
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-09 15:50:33 +0000

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the function `is_same_class`"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns True if the object is exactly an instance of the specified class
+    otherwise False
+    """
+    return type(obj) == a_class
