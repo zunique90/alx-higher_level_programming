630c661e19a0a10170401bcb39f9f58994cd2e99
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-09 03:35:45 -0800

    removed blank line

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,6 @@ This module `1-my_list` defines a class
 class MyList(list):
     """This is a child of the parent class `list`"""
 
-
     def print_sorted(self):
         """a public instance method that prints the list
         but sorted (ascending sort)"""
