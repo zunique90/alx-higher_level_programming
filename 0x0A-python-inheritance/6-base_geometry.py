7fbbd326ab3b9236c7688537cad37d23af1c0416
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-09 16:17:24 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """a public instance method that is not implemented"""
+        raise Exception("area() is not implemented")
