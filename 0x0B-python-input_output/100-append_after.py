981b40e914e9070ca156fb5c947419b571e8500f
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-10 18:59:51 +0000

    advanced task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines the `append_after` function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file
+    after each line containing a specific string
+    """
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
+
