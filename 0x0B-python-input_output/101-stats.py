929a8e9e360c414a28879153cc26373efe219eb0
charles ezebuike <ezebuike.charles0@gmail.com>
2023-01-10 19:24:51 +0000

    advanced task 14

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This script reads stdin line by line and computes metrics"""
+
+
+import sys
+from collections import defaultdict
+
+
+def print_stats():
+    """prints statistics"""
+    print("Total file size: ", total_size)
+    for status_code in sorted(status_codes.keys()):
+        print(f"{status_code}: {status_codes[status_code]}")
+
+
+if __name__ == "__main__":
+    line_count = 0
+    total_size = 0
+    status_codes = defaultdict(int)
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+
+            fields = line.strip().split(" ")
+
+            total_size += int(fields[-1])
+
+            status_codes[fields[-2]] += 1
+
+            if line_count % 10 == 0:
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()
