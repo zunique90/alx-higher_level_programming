fe00e518292408ab29fe389d93f658514e92263d
charles ezebuike <ezebuike.charles0@gmail.com>
2023-02-14 20:55:05 +0000

    tasks 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- This script lists all records of the table second_table
+-- Results should display both the score and the name
+-- Records should be ordered by score (top first)
+SELECT score, name FROM second_table ORDER BY score DESC;
