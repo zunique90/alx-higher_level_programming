fa209b4a30f0d250971138a44088a0f03f1ddab0
charles ezebuike <ezebuike.charles0@gmail.com>
2023-02-15 20:58:36 +0000

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- This script creates a new user that has all privileges
+REATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
