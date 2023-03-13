7565d9d82495e40b6b1f14e46f0b285bafe1a829
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-13 21:28:37 +0000

    task 6a

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let idx = 0;
 while (idx < arr.length) {
   console.log(arr[idx]);
