855d59a1acf91f71cff76f494126a844944f2999
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-14 21:50:22 +0000

    task 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const dict1 = require('./101-data').dict;
-let res = {};
-for (let key in dict1) {
+const res = {};
+for (const key in dict1) {
   if (res[dict1[key]] === undefined) {
     res[dict1[key]] = [key];
   } else {
-  res[dict1[key]].push(key);
-
+    res[dict1[key]].push(key);
   }
 }
 console.log(res);
