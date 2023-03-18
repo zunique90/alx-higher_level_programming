b18d505fb2f52411b7a5859db2fa354d5da4b3d3
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-18 13:38:35 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all states from a database"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2],
+        db=argv[3], charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE BINARY name='{:s}' ORDER BY id"
+            .format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
