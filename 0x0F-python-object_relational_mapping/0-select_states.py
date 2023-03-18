b4dbffaf67dacf138f066dc109691c12b1e32fc1
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-18 12:44:48 +0000

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import MySQLdb
+
+
+db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="ezebuike", db="hbtn_0e_0_usa")
+cur = db.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+db.close()
