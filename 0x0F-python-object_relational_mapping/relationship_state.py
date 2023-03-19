6e09fa67953b254e9f45ef6985adf53a64317b95
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-19 05:49:50 -0700

    task 15

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,9 +5,7 @@ This module defines a state Model
 
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, Integer, String
-from relationship_city import City
-
-Base = declarative_base()
+from relationship_city import Base, City
 
 
 class State(Base):
