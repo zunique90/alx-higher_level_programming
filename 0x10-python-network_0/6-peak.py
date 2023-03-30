acf3ad68175ce22ac3e0039473002aec0287ebdc
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-30 19:06:58 +0000

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,5 +13,5 @@ def find_peak(list_of_integers):
                 if (list_of_integers[i] >= list_of_integers[i - 1] and
                    list_of_integers[i] >= list_of_integers[i + 1]):
                     return list_of_integers[i]
-            except:
+            except Exception:
                 return list_of_integers[len(list_of_integers)]
