d6f9761b5b3afe1a1de225ff672da5804c17c269
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-30 19:10:34 +0000

    tech interview prep

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" find peak"""
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    if len(list_of_integers) == 0:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    if list_of_integers[0] >= list_of_integers[1]:
-        return (list_of_integers[0])
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
     else:
-        for i in range(1, len(list_of_integers)):
-            try:
-                if (list_of_integers[i] >= list_of_integers[i - 1] and
-                   list_of_integers[i] >= list_of_integers[i + 1]):
-                    return list_of_integers[i]
-            except Exception:
-                return list_of_integers[len(list_of_integers)]
+        return find_peak(list_of_integers[mid + 1:])
