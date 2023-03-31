1edf75072b76d85753e6f8a16781ae871edb274a
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-31 21:58:48 +0000

    push for test in sandbox

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,11 +8,11 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    if len(argv) < 1:
-        val = {"q": ""}
-    else:
+    if len(argv) > 1:
         args = argv
         val = {"q": args[1]}
+    else:
+        val = {"q": ""}
     try:
         req = r.post("http://0.0.0.0:5000/search_user", data=val)
         datas = req.json()
