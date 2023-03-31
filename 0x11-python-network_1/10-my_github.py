04e036ff9febe0b6ac167b05f2eab900ac82bebf
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-31 22:14:22 +0000

    task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes Github Credentials and displays user id
+using the GitHub API
+"""
+
+from sys import argv
+import requests as r
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ =="__main__":
+    userAuth = HTTPBasicAuth(argv[1], argv[2])
+    response = r.get("https://api.github.com/user", auth=userAuth)
+    userId = response.json()
+    print(userId.get("id"))
