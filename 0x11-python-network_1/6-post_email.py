1ae2c34c7f4e3ef2b3f4ae2351c3fcca5ba27542
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-31 21:29:55 +0000

    push for test in sandbox

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response.
+"""
+
+from sys import argv
+import request
+
+
+if __name__ == "__main__":
+    args = argv
+    val = {"email": args[2]}
+    req = requests.post(args[1], data=val)
+        print(req.text)
