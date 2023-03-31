4e52a241ab97ff071d4c779680743767dbdcb067
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-31 21:02:57 +0000

    push task 3 to test in sandbox

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script sends a request to the URL
+and displays the body of the response (decoded in utf-8)
+"""
+
+from sys import argv
+from urllib.request import Request, urlopen
+from urlib.error import HTTPError
+
+
+if __name__ == "__main__":
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as error:
+        print("Error code: {}".format(error.code))
